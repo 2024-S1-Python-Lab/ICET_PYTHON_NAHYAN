3f4e68bddd7c3836b359dc6e2ac98c16ba80f9ba
nahyan-icet <nahyanicet@gmail.com>
2024-12-11 11:59:05 +0530

    Add files via upload

---
+++ q1.py.py
@@ -0,0 +1,11 @@
+final_year = int(input("Enter the final year: "))
+current_year = int(input("Enter the c year: "))
+
+if final_year < current_year:
+    print("Final year must be greater than or equal to the current year")
+
+else:
+    print (f"Leap years from {current_year} to {final_year}")
+    for year in range(current_year, final_year + 1):
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+            print(year)
